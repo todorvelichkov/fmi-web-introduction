b3e2693c2411d60963ee7455e5e258bc6ee3364f
Todor Velichkov <todorvelichkov89@gmail.com>
2018-04-15 12:06:17 +0300

    Refacture

--- upr1/simple-server.py
+++ upr1/simple-server.py
@@ -1,9 +1,9 @@
 import socket
-import typing
 
 HOST = "127.0.0.1"
 PORT = 9000
 
+#resources: https://defn.io/2018/02/25/web-app-from-scratch-01/
 
 def iter_lines(sock, bufsize=16384):
     """Given a socket, read all the individual CRLF-separated lines
@@ -44,19 +44,23 @@ with socket.socket() as server_sock:
     print("Listening on {HOST}:{PORT}...".format(HOST=HOST, PORT=PORT))
 
     while True:
-        # process upcoming connection
+        # it’ll print to standard out that it’s listening on 127.0.0.1:9000 and then exit.
+        # In order to actually process incoming connections we need to call the accept method on our socket.
+        # Doing so will block the process until a client connects to our server.
         client_sock, client_addr = server_sock.accept()
         print("New connection from {client_addr}.".format(client_addr=client_addr))
 
+        # Once we have a socket connection to the client, we can start to communicate with it.
+        # Using the sendall method, let’s send the connecting client an example response:
         with client_sock:
-            for request_line in iter_lines(client_sock):
-                print(request_line)
+            lines = [request_line for request_line in iter_lines(client_sock)]
+            request_path = lines[0].decode().split(' ')[1]
 
             response = '\r\n'.join([
                 'HTTP/1.1 200 OK',
                 'Content-type: text/html',
-                'Content-length: 15',
+                # 'Content-length: 15',
                 '',
-                '<h1>Hello %s!</h1>' % client_addr[1],
+                '<h1>Hello %s You are at %s</h1>' % (client_addr[1], request_path)
             ])
             client_sock.sendall(response.encode())
