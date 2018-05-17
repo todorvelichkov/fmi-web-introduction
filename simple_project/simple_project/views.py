5be2e69dab513dc6e2e4e61dadb4991993b4f16e
Todor Velichkov <todorvelichkov89@gmail.com>
2018-05-18 00:45:54 +0300

    authentication using django views

--- simple_project/simple_project/views.py
+++ simple_project/simple_project/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index_view(request):
-	return HttpResponse("This is my home page.", content_type="text/plain")
+    return HttpResponse("This is my home page.", content_type="text/plain")
